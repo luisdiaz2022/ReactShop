0ff3f2e134e95a3d81bc5d7afb9ab1afb55c748a
Luis Diaz <luisedgucv@gmail.com>
2023-01-26 17:44:39 -0500

    Second commit, adding Webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const { dirname } = require('path');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_module/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: 'html-loader'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        }
+        )
+    ]
+}
